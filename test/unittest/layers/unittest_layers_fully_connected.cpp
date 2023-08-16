b32e46fa855707b110cd97d6c362bd4cbc6ecbae
Adwaith Anand <adwaith.a@samsung.com>
2023-08-16 14:13:44 +0530

    [FullyConnected] Added NHWC support for FC_Layer backwarding part.
    This also contains changes in unit tests to evaluate the same.
    
    **Self evaluation:**
        1. Build test:   [X]Passed [ ]Failed [ ]Skipped
        2. Run test:     [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: Adwaith Anand <adwaith.a@samsung.com>

--- test/unittest/layers/unittest_layers_fully_connected.cpp
+++ test/unittest/layers/unittest_layers_fully_connected.cpp
@@ -41,25 +41,19 @@ auto fc_basic_no_decay = LayerGoldenTestParamType(
 
 auto fc_basic_plain_nhwc = LayerGoldenTestParamType(
   nntrainer::createLayer<nntrainer::FullyConnectedLayer>, {"unit=5"},
-  "3:10:1:1", "fc_plain.nnlayergolden",
-  LayerGoldenTestParamOptions::SKIP_CALC_DERIV |
-    LayerGoldenTestParamOptions::SKIP_CALC_GRAD,
+  "3:10:1:1", "fc_plain.nnlayergolden", LayerGoldenTestParamOptions::DEFAULT,
   "nhwc", "fp32", "fp32");
 
 auto fc_basic_single_batch_nhwc = LayerGoldenTestParamType(
   nntrainer::createLayer<nntrainer::FullyConnectedLayer>, {"unit=4"},
   "1:10:1:1", "fc_single_batch.nnlayergolden",
-  LayerGoldenTestParamOptions::SKIP_CALC_DERIV |
-    LayerGoldenTestParamOptions::SKIP_CALC_GRAD,
-  "nhwc", "fp32", "fp32");
+  LayerGoldenTestParamOptions::DEFAULT, "nhwc", "fp32", "fp32");
 
 auto fc_basic_no_decay_nhwc = LayerGoldenTestParamType(
   nntrainer::createLayer<nntrainer::FullyConnectedLayer>,
   {"unit=5", "weight_decay=0.0", "bias_decay=0.0"}, "3:10:1:1",
-  "fc_plain.nnlayergolden",
-  LayerGoldenTestParamOptions::SKIP_CALC_DERIV |
-    LayerGoldenTestParamOptions::SKIP_CALC_GRAD,
-  "nhwc", "fp32", "fp32");
+  "fc_plain.nnlayergolden", LayerGoldenTestParamOptions::DEFAULT, "nhwc",
+  "fp32", "fp32");
 
 GTEST_PARAMETER_TEST(FullyConnected, LayerGoldenTest,
                      ::testing::Values(fc_basic_plain, fc_basic_single_batch,
